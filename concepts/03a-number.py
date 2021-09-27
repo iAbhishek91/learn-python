0e35509271b47b3940ae61801957610543522941
Abhishek Das <abhishek.das@complyadvantage.com>
2021-09-27 08:42:02 +0100

    exception handling

--- concepts/03a-number.py
+++ concepts/03a-number.py
@@ -4,8 +4,12 @@ import random
 #unlimited size
 int1 = 9987098273049582734095872309847502983475
 int2 = -2098374509827345
+int3 = 0b1111 # in binary
+int4 = 0o7654 # in octal
+int5 = 0x9876 # in hexadecimal
 print(int1)  # OUTPUT: 9987098273049582734095872309847502983475
 print(-int2)  # OUTPUT: 2098374509827345
+print(int3, int4, int5) # OUTPUT: 15, 4012 39030 they always output as decimal
 
 
 
